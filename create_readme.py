2645e5e59f27d28094b24038b6b4308c1e33c70e
Anyesh <sthacruz@gmail.com>
2023-01-28 17:35:53 +1100

    Update URL

--- create_readme.py
+++ create_readme.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import re
-from urllib.parse import quote_plus
 
 ROOTDIR = Path(__file__).parent
 README = ROOTDIR / "README.md"
@@ -22,7 +21,12 @@ def create_readme():
             if path.stem not in [x[0] for x in existing_explains if x]:
                 raw_url = f"https://github.com/Anyesh/explains/blob/master/{path.name}"
 
-                readme.write(f"- [{path.stem}]({quote_plus(raw_url)})\n")
+                readme.write(f"- [{path.stem}]({build_url(raw_url)})\n")
+
+
+def build_url(raw_url):
+    """Build a GitHub URL from a raw URL."""
+    return "%20".join(raw_url.split(" "))
 
 
 if __name__ == "__main__":
