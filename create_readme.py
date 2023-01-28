2cac08ed9576d615bd935a8c61dd90bc8b39d032
Anyesh <sthacruz@gmail.com>
2023-01-28 17:25:08 +1100

    Automate README creation

---
+++ create_readme.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import re
+from urllib.parse import quote_plus
+
+ROOTDIR = Path(__file__).parent
+README = ROOTDIR / "README.md"
+
+FILE_EXT = ".md"
+
+
+def create_readme():
+    """Create README.md file from all .md files in the current directory."""
+    with README.open("r+") as readme:
+        existing_explains_md = readme.readlines()
+        existing_explains = [
+            re.findall(r"-\s\[(.+)\]\(", x) for x in existing_explains_md
+        ]
+
+        for path in ROOTDIR.glob(f"*{FILE_EXT}"):
+            if path.name == "README.md":
+                continue
+            if path.stem not in [x[0] for x in existing_explains if x]:
+                raw_url = f"https://raw.githubusercontent.com/Anyesh/explains/master/{path.name}"
+
+                readme.write(f"- [{path.stem}]({quote_plus(raw_url)})\n")
+
+
+if __name__ == "__main__":
+    create_readme()
